aaae1c86d19cc390dfdfae303ba367a1cffbc2f1
Andrew Barba <abarba.77@gmail.com>
2014-07-07 00:03:27 -0400

    relationship tests

--- app/services/relationship.js
+++ app/services/relationship.js
@@ -9,7 +9,7 @@ var utils = spot.utils
 exports.formRelationships = function(from, to, group, next) {
 	
 	var contacts = _.indexBy(to, 'user');
-	delete contacts[from];
+	// delete contacts[from];
 
 	var userIds = Object.keys(contacts);
 
@@ -18,11 +18,12 @@ exports.formRelationships = function(from, to, group, next) {
 	utils.asyncStream(
 		query,
 		function(doc, next){
-			var contact = contacts[doc.id];
+			var userId = doc.to;
+			var contact = contacts[userId];
 			doc.nickname = contact.nickname || doc.nickname;
 			doc.group = group;
 			doc.save(next);
-			delete contacts[doc.id];
+			delete contacts[userId];
 		},
 		function(err){
 			if (err) return next(err);
@@ -36,13 +37,10 @@ exports.formRelationships = function(from, to, group, next) {
 				};
 			});
 
-			Relationship
-				.create(data)
-				.populate('to')
-				.exec(function(err){
-					if (err) return next(err);
-					var docs = _.values(arguments).slice(1);
-					next(null, docs);
-				});
+			Relationship.create(data, function(err){
+				if (err) return next(err);
+				var docs = _.values(arguments).slice(1);
+				next(null, docs);
+			});
 		});
 }
--- test/controllers/relationship.js
+++ test/controllers/relationship.js
@@ -9,21 +9,47 @@ var request = require('supertest')
 describe('Relationship', function(){
 
 	describe('/relationship create', function(){
-		var user = spot.test.users[0];
-		var auth = spot.test.auths[user.id].token;
+		var userId = spot.test.users[0].id;
+		var auth = spot.test.auths[userId].token;
+
+		var groupId = spot.test.groups[userId][0].id; 
+		var users = _.map(spot.test.users, function(user){
+			return {
+				user: user.id,
+				nickname: utils.randomHex(10)
+			};
+		});
+		var body = {
+			group: groupId,
+			users: users
+		};
 
 		it('should create new relationships', function(done){
-			var body = _.map(spot.test.users, function(user){
-				return {
-					user: user.id,
-					nickname: utils.randomHex(10)
-				};
-			});
-			done();
+			server
+				.post('/relationship')
+				.query({ auth: auth })
+				.send(body)
+				.expect(200)
+				.end(function(err, res){
+					should.not.exist(err);
+					should.exist(res);
+					res.body.length.should.not.equal(0);
+					done();
+				});
 		});
 
 		it('should not create any new relationships', function(done){
-			done();
+			server
+				.post('/relationship')
+				.query({ auth: auth })
+				.send(body)
+				.expect(200)
+				.end(function(err, res){
+					should.not.exist(err);
+					should.exist(res);
+					res.body.length.should.equal(0);
+					done();
+				});
 		});
 	});
 });
