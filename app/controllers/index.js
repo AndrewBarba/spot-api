ebca38facb9ed9289aabfdc5095a8b1778015998
Andrew Barba <abarba.77@gmail.com>
2014-07-08 16:39:23 -0400

    more cleanup

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -49,7 +49,7 @@ module.exports = function(app) {
 
 	// handle errors
 	app.use(function(err, req, res, next){
-		if (!spot.config.env.TEST) {
+		if (!config.env.TEST) {
 			spot.err(new Date(), req.method, req.url, err);
 		}
 		Error.send(err, res);
